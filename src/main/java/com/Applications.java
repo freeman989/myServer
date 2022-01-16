488c825f61cfac910d35ec646a2f23ea7707071c
Yuriy Vins <yuriy.vins83@gmail.com>
2022-01-16 17:48:10 +0600

    'My third commit'

--- src/main/java/com/Applications.java
+++ src/main/java/com/Applications.java
@@ -2,7 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Test Swagger
+
+
 @SpringBootApplication
 public class Applications {
 
